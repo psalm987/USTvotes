909da1b5249e4a6c817d9d4d8d750ed852b0da5f
psalm987 <samrythmn@gmail.com>
2022-02-09 10:06:07 +0100

    Section 1 Home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,55 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import ThemeContext from '../components/states/theme/themeContext.js';
 
-export default function Home() {
+function Section1() {
   const { toggleTheme } = useContext(ThemeContext);
   return (
-    <Button onClick={toggleTheme} variant="contained">
-      Click me to change the background
-    </Button>
+    <Box p={0} m={0}>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} md={6} sx={{ my: { xs: 2, md: 8 } }}>
+            <Typography variant="h2" gutterBottom>
+              Make a statement, cast your vote
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+              Your voice is your vote, show solidarity to the most qualified
+              leader by casting a vote in. their favor.
+            </Typography>
+            <Button
+              variant="contained"
+              disableElevation
+              sx={{ textTransform: 'none', mt: 2 }}
+              onClick={toggleTheme}
+            >
+              View candidates
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                backgroundColor: 'secondary.main',
+                height: 1,
+                width: 1,
+                minHeight: '300px',
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Section1 />
+    </React.Fragment>
   );
 }
