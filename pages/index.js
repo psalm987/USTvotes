bd8cd7379312a6b3fc29ca1bf96165ef168a2b99
psalm987 <samrythmn@gmail.com>
2022-02-09 14:10:16 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ThemeContext from '../components/states/theme/themeContext.js';
 
-function Section1() {
+function Headline() {
   const { toggleTheme } = useContext(ThemeContext);
   return (
     <Box p={0} m={0}>
       <Container>
         <Grid container>
-          <Grid item xs={12} md={6} sx={{ my: { xs: 2, md: 8 } }}>
+          <Grid item xs={12} md={6} sx={{ my: { xs: 8, md: 16 } }}>
             <Typography variant="h2" gutterBottom>
               Make a statement, cast your vote
             </Typography>
@@ -46,10 +46,114 @@ function Section1() {
   );
 }
 
+function Countdown() {
+  const [Dday, setDday] = React.useState(new Date(2023, 5, 5));
+  const [display, setDisplay] = React.useState({
+    day: 0,
+    hour: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setDisplay((prevDisplay) => {
+        const currentTime = new Date();
+        if (currentTime > Dday) {
+          return {
+            day: 0,
+            hour: 0,
+            minutes: 0,
+            seconds: 0,
+          };
+        }
+        const diff = new Date(Dday - currentTime);
+        return {
+          day: Math.floor(diff.getTime() / (1000 * 60 * 60 * 24)),
+          hour: diff.getHours(),
+          minutes: diff.getMinutes(),
+          seconds: diff.getSeconds(),
+        };
+      });
+    }, 1000);
+  }, []);
+
+  if (Object.values(display).every((i) => i === 0)) return <React.Fragment />;
+
+  return (
+    <Box py={{ xs: 8, md: 16 }}>
+      <Container>
+        <Typography variant="h4" gutterBottom textAlign="center" sx={{ mb: 4 }}>
+          Countdown to the next Election
+        </Typography>
+        <Grid spacing={4} container justifyContent="center" alignItems="center">
+          <Grid item xs={6} md={3}>
+            <Typography variant="h3" textAlign="center" gutterBottom>
+              {display.day}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              textAlign="center"
+              component="p"
+              sx={{ textTransform: 'uppercase' }}
+            >
+              days
+            </Typography>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <Typography variant="h3" textAlign="center" gutterBottom>
+              {display.hour}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              textAlign="center"
+              component="p"
+              sx={{ textTransform: 'uppercase' }}
+            >
+              hours
+            </Typography>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <Typography variant="h3" textAlign="center" gutterBottom>
+              {display.minutes}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              textAlign="center"
+              component="p"
+              sx={{ textTransform: 'uppercase' }}
+            >
+              minutes
+            </Typography>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <Typography variant="h3" textAlign="center" gutterBottom>
+              {display.seconds}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              textAlign="center"
+              component="p"
+              sx={{ textTransform: 'uppercase' }}
+            >
+              seconds
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
 export default function Home() {
   return (
     <React.Fragment>
-      <Section1 />
+      <Headline />
+      <Countdown />
     </React.Fragment>
   );
 }
