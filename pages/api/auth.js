dfd011ce05fa049a84d8b2e53114f7eddec1b8e7
psalm987 <samrythmn@gmail.com>
2022-02-18 12:10:07 +0100

    4 files modified

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -4,14 +4,30 @@ import Staff from '../../lib/models/Staff.js';
 export default async function handler(req, res) {
   const { method } = req;
 
+  console.log('Connecting to DB');
   await connectToDatabase();
+
+  console.log('Connected!');
 
   switch (method) {
     case 'GET':
-      const AllStaff = await Staff.find();
-      return res
-        .status(200)
-        .json({ success: true, message: 'Successful!', data: AllStaff });
+      try {
+        const allStaff = Staff.find({
+          name: 'Samuel Seibidor',
+        });
+        return res.status(200).json({
+          success: true,
+          message: 'Successful!',
+          data: allStaff || [],
+        });
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          success: false,
+          message: 'Internal server error.',
+          data: null,
+        });
+      }
     default:
       return res.status(400).json({
         success: false,
